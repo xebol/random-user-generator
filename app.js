4e59cf262c3a52877d85ecda1d799c4a2a9f1550
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-31 12:59:07 -0600

    add alternative way to request API using axios and promise

--- app.js
+++ app.js
@@ -14,11 +14,26 @@ const app = Vue.createApp({
         fetch('https://randomuser.me/api');
       const { results } = await res.json();
 
-      this.firstName = results[0].name.first
-      this.lastName = results[0].name.last
-      this.email = results[0].email
-      this.gender = results[0].gender
-      this.picture = results[0].picture.large
+
+      //how to use axios and promise instead of async and await and fetch
+      // function getUser() {
+      //   axios.get('https://randomuser.me/api')
+      //     .then((response) => {
+      //       const { results } = response.data;
+      //       // Process the data as needed
+      //       console.log(results);
+      //     })
+      //     .catch((error) => {
+      //       // Handle error
+      //       console.error(error);
+      //     });
+      // }
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
     }
   }
 });
